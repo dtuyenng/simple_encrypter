f700859c3e11741822a4a1eb115631f4e82c05f9
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-10-14 12:51:47 -0700

    Commit
    Commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import base64
+from os.path import expanduser
+
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -8,10 +10,11 @@ import json
 import customtkinter as ctk
 from tkinter import filedialog
 
+from setuptools.monkey import patch_all
+
+
 def main():
     app = Application()
-    # frame = Frame(app)
-    file_selector = FileSelector(app)
     app.mainloop()
 
 
@@ -21,18 +24,48 @@ class Application(ctk.CTk):
         self.geometry('600x400')
         self.configure(fg_color="white")
 
+        self.file_selector = FileSelector(self)
+        self.password_input = PasswordInput(self)
+
+
+
+class PasswordInput(ctk.CTkFrame):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs, fg_color="pink")
+        self.pack(expand=True, fill="x", padx=10, pady=10)
+
+        self.password_label1 = ctk.CTkEntry(self, placeholder_text="Enter Password", width=200)
+        self.password_label1.pack( padx=10, pady=10,)
+        self.password_label2 = ctk.CTkEntry(self, placeholder_text="Confirm Password", width=200)
+        self.password_label2.pack(padx=10, pady=10, )
+
+        self.encrypt_button = ctk.CTkButton(self, text="Encrypt File", command=self.encrypt,
+                                            width=150,
+                                            height=50)
+        self.encrypt_button.pack()
+
+    def encrypt(self):
+        password1 = self.password_label1.get()
+        password2 = self.password_label2.get()
+        if password1 != password2:
+            print("Passwords don't match, try again")
+            self.password_label1.delete(0, ctk.END)
+            self.password_label2.delete(0, ctk.END)
+            self.password_label1.focus()
+
 
 class FileSelector(ctk.CTkFrame):
     def __init__(self, parent, **kwargs):
-        super().__init__(parent, fg_color = "lightblue", **kwargs)
+        super().__init__(parent,  **kwargs, fg_color = "lightblue")
         self.pack(expand=True, fill="x", padx=10, pady=10)
         self.label = ctk.CTkLabel(self, text="Pick a file...",
                                   anchor="w",
                                   font=("Arial", 16),
                                   fg_color="white")
         self.label.pack(side="left", padx=10, pady=10, expand=True, fill="x")
-        self.button = ctk.CTkButton(self, text="Select File", command=self.pick_file)
-        self.button.pack(side="right", padx=10, pady=10)
+        self.button = ctk.CTkButton(self, text="Select File", command=self.pick_file,
+                                    width=70)
+        self.button.pack(side="right", padx=10)
 
 
     def pick_file(self, event=None):
