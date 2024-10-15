8834747ddf08b524b6c23fd4a6694759cf2b5bfe
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-10-15 10:11:46 -0700

    Commit
    commit

--- main.py
+++ main.py
@@ -1,73 +1,10 @@
-import base64
-from os.path import expanduser
-
-from cryptography.fernet import Fernet, InvalidToken
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from pathlib import Path
-import json
-
+import Encrypter as Encrypter
 import customtkinter as ctk
 from tkinter import filedialog
-
-from setuptools.monkey import patch_all
-
 
 def main():
     app = Application()
     app.mainloop()
-
-class Encrypter:
-    def __init__(self):
-        self.salt = self.import_salt()
-
-    @staticmethod
-    def import_salt() -> str:
-        json_data = json.loads(Path("config.json").read_text())
-        print("Config file loaded.")
-        return json_data["salt"]
-
-    def generate_key(self, password: str) -> bytes:
-        kdf = PBKDF2HMAC(
-            algorithm = hashes.SHA256(),
-            length = 32,
-            salt = self.salt.encode(),
-            iterations = 100_000,
-        )
-        return base64.urlsafe_b64encode(kdf.derive(password.encode()))
-
-    def encrypt_file(self, file_path: str, input_password: str):
-        key = self.generate_key(input_password)
-        fernet = Fernet(key)
-
-        read_path = Path(file_path)
-        binary_data = read_path.read_bytes()
-
-        encrypted_data = fernet.encrypt(binary_data)
-
-        write_path = Path('encrypted_data.bin')
-        write_path.write_bytes(encrypted_data)
-        print(f"File encrypted and saved to: {write_path}")
-
-    def decrypt_file(self, file_path: str, input_password: str):
-        try:
-            key = self.generate_key(input_password)
-            fernet = Fernet(key)
-
-            write_path = Path(file_path)
-            encrypted_data = write_path.read_bytes()
-
-            decrypted_data = fernet.decrypt(encrypted_data)
-
-            write_path = Path("decrypted_data.rename")
-            write_path.write_bytes(decrypted_data)
-            print("Decryption Successful: 'decrypted_data.rename' created.")
-        except FileNotFoundError:
-            print(f"File not found.")
-        except InvalidToken:
-            print("Decryption failed. The password might be incorrect or the data might be corrupted.")
-
-
 
 
 class Application(ctk.CTk):
@@ -76,10 +13,14 @@ class Application(ctk.CTk):
         self.geometry('600x400')
         self.configure(fg_color="white")
 
-        self.file_selector = FileSelector(self)
+        """ Encrypter """
+        self.label1 = ctk.CTkLabel(self, text="Encrypter").pack()
+        self.file_selector = TheEncrypter(self)
         self.password_input = PasswordInput(self, self.file_selector)
 
-class FileSelector(ctk.CTkFrame):
+        """ Decrypter """
+
+class TheEncrypter(ctk.CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent,  **kwargs, fg_color = "lightblue")
         self.pack(expand=True, fill="x", padx=10, pady=10)
